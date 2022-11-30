40e5bff9b60fbbb1d3b46c6a0ba840fc42c0b0e9
bigliolimatteo <bigliolimatteo@gmail.com>
2022-11-30 23:10:33 +0100

    some new pieces to the main

--- src/main.py
+++ src/main.py
@@ -1,22 +1,26 @@
-import utils.istat_downloader as istat_downloader 
+from utils.istat_manager import IstatManager
 
 import argparse
 import os
+import geopandas as gpd
 
-def main():
-    istat_downloader.download()
+def main(input_coverage_shapefile):
+    coverage_gdf = gpd.read_file(input_coverage_shapefile)
+    coverage_polygon = list(coverage_gdf["geometry"])[0]
+
+    istat_manager = IstatManager()
+    covered_cities = istat_manager.get_covered_cities(coverage_polygon)
+    print(covered_cities)
 
 if __name__ == "__main__":
    
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-i", "--input_coverage", required=True, type=str, help="Path to a shapefile containing the coverage of interest")
+    parser.add_argument("-i", "--input_coverage_shapefile", required=True, type=str, help="Path to a shapefile containing the coverage of interest")
     
     args = parser.parse_args()
     
-    if not args.input_coverage.endswith("shp") or not os.path.exists(args.input_coverage):
-        parser.error("input_coverage must be an existing shapefile.")
+    if not args.input_coverage_shapefile.endswith("shp") or not os.path.exists(args.input_coverage_shapefile):
+        parser.error("input_coverage_shapefile must be an existing shapefile.")
     
-    print(args)
-
-    main()
+    main(args.input_coverage_shapefile)
