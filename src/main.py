7586b117603d3026369b80f700eb5ee055119d94
bigliolimatteo <bigliolimatteo@gmail.com>
2022-12-01 18:26:04 +0100

    download buildings in main

--- src/main.py
+++ src/main.py
@@ -1,16 +1,26 @@
 from utils.istat_manager import IstatManager
+import utils.pcm_manager as pcm_manager
+import utils.shapefile_manager as shapefile_manager
 
 import argparse
 import os
 import geopandas as gpd
+import pandas as pd
 
 def main(input_coverage_shapefile):
     coverage_gdf = gpd.read_file(input_coverage_shapefile)
     coverage_polygon = list(coverage_gdf["geometry"])[0]
 
     istat_manager = IstatManager()
-    covered_cities = istat_manager.get_covered_cities(coverage_polygon)
-    print(covered_cities)
+    covered_towns = istat_manager.get_covered_towns(coverage_polygon)
+    covered_towns_istat_info = istat_manager.get_istat_info_from_town_codes(covered_towns["PRO_COM"].values)
+    covered_buildings_gdfs = list()
+    for _, town_data in covered_towns_istat_info.iterrows():   
+        tmp_town_gdf = pcm_manager.download_town_buildings_shp(town_data["area_istat_name"], town_data["region_name"], town_data["province_name"], town_data["town_name"])
+        covered_buildings_gdfs.append(shapefile_manager.filter_intersection_geometries(coverage_polygon, tmp_town_gdf))
+    covered_buildings_gdf = gpd.GeoDataFrame(pd.concat(covered_buildings_gdfs, ignore_index=True))
+    print(covered_buildings_gdf)
+    covered_buildings_gdf.to_file("test/extraction.shp")
 
 if __name__ == "__main__":
    
