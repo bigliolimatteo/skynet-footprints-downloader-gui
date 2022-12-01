6088be088eed33726f6cdc048d60c8fedb1facac
bigliolimatteo <bigliolimatteo@gmail.com>
2022-12-01 18:52:36 +0100

    update gitignore and add province mapping

--- src/utils/pcm_manager.py
+++ src/utils/pcm_manager.py
@@ -11,15 +11,20 @@ area_mapping = {"Nord-ovest": "ITC-NordOvest",
                 "Sud": "ITF-Sud",
                 "Isole": "ITG-Isole"}
 
+province_mapping = {"Monza_e_della_Brianza": "MonzaBrianza"}
+
 def download_town_buildings_shp(area_istat_name, region_name, province_name, town_name):
     area_pcm_name = area_mapping[area_istat_name].split("-")[1]
     area_acronym = area_mapping[area_istat_name].split("-")[0]
+    mapped_province_name = province_mapping.get(province_name.replace(" ", "_"), province_name.replace(" ", "_"))
     path = gh_path.format(gh_prefix=gh_prefix, 
                         gh_repo_prefix=gh_repo_prefix,
                         area_acronym=area_acronym,
                         area_pcm_name=area_pcm_name,
-                        region_name=region_name,
-                        province_name=province_name,
-                        town_name=town_name)
+                        region_name=region_name.replace(" ", "_"),
+                        province_name=mapped_province_name,
+                        town_name=town_name.replace(" ", "_"))
+
+    print(path)
     return gpd.read_file(f"/vsicurl/{path}").to_crs("EPSG:4326")
     
